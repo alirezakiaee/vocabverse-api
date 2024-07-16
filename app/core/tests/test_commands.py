5116d2dfa742ba0dcd36ead94c76fb347c6c472f
akphisher <alirezakiaee91@gmail.com>
2024-07-16 14:14:56 -0400

    fixed some testing and linting issues- muted qa in admin.py and models.py

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -9,14 +9,15 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
-@patch('core.management.commands.wait_for_db.Command.check') # Mock the check method
+
+@patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test the custom management commands"""
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for db when db is ready"""
         patched_check.return_value = True
         call_command('wait_for_db')
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
@@ -25,4 +26,5 @@ class CommandTests(SimpleTestCase):
             [OperationalError] * 3 + [True]
         call_command('wait_for_db')
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=['default'])
+
+        patched_check.assert_called_with(databases=['default'])
