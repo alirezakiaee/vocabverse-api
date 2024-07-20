1f53fa5fdee17c3e1fae65ac478c4c66032ebd35
alirezakiaee <alirezakiaee91@gmail.com>
2024-07-19 22:59:52 -0400

    added the users app and configured user creation api

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -15,12 +15,12 @@ class AdminSiteTests(TestCase):
         """Create user and client."""
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email='user@example.com',
+            email='user222@example.com',
             password='testpass123',
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email='user@example.com',
+            email='user12@example.com',
             password='testpass123',
             name='Test User',
         )
